78bf541e747e8b932580f23ea010a4e647f94fbd
Wonjoon Cho <cwj3688@gmail.com>
2025-12-08 06:30:55 +0000

    테라폼 프로바이더 버전 업데이트

--- provider.tf
+++ provider.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     samsungcloudplatformv2 = {
-      version = "1.0.3"
+      version = "3.0.3"
       source = "samsungsdscloud/samsungcloudplatformv2"
     }
     time = {
